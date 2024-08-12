81bcee5981437f866085efedce4c203cbaca3e74
cs-shubham-morankar <smorankar@fortinet.com>
2024-08-12 15:05:44 +0530

    First Commit

---
+++ cyberark-aim/operations.py
@@ -0,0 +1,145 @@
+""" Copyright start
+  Copyright (C) 2008 - 2024 Fortinet Inc.
+  All rights reserved.
+  FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+  Copyright end """
+
+import requests
+from requests_ntlm import HttpNtlmAuth
+from connectors.core.connector import get_logger, ConnectorError
+
+logger = get_logger('cyberark-aim')
+
+
+class CyberARK:
+    def __init__(self, config):
+        self.base_url = config['server_url'].strip("/")
+        if not self.base_url.startswith('https://'):
+            self.base_url = 'https://{0}'.format(self.base_url)
+        self.verify_ssl = config.get('verify_ssl')
+        self.auth_type = config.get('auth_type')
+        if self.auth_type == 'Basic Auth':
+            self.username = config.get('username')
+            self.password = config.get('password')
+            self.auth = HttpNtlmAuth(self.username, self.password)
+        self.headers = {'Content-Type': 'application/json'}
+
+    def make_request_call(self, endpoint, payload=None, query_params=None, method='GET'):
+        url = '{base}{endpoint}'.format(base=self.base_url, endpoint=endpoint)
+        try:
+            if self.auth_type == 'No Auth':
+                response = requests.request(method, url, data=payload, params=query_params, headers=self.headers,
+                                            verify=self.verify_ssl)
+            else:
+                response = requests.request(method, url, data=payload, params=query_params, auth=self.auth,
+                                            headers=self.headers,
+                                            verify=self.verify_ssl)
+            logger.debug("Response: {0}".format(response.text))
+            if response.ok or response.status_code == 204:
+                logger.info('Successfully got response for url {0}'.format(url))
+                if 'json' in str(response.headers):
+                    return response.json()
+                else:
+                    return response
+            else:
+                logger.debug("response_content {0}:{1}".format(response.status_code, response.content))
+                raise ConnectorError("{0}:{1}".format(response.status_code, response.text))
+        except requests.exceptions.SSLError:
+            raise ConnectorError('SSL certificate validation failed')
+        except requests.exceptions.ConnectTimeout:
+            raise ConnectorError('The request timed out while trying to connect to the server')
+        except requests.exceptions.ReadTimeout:
+            raise ConnectorError(
+                'The server did not send any data in the allotted amount of time')
+        except requests.exceptions.ConnectionError:
+            raise ConnectorError('Invalid Credentials')
+        except Exception as err:
+            raise ConnectorError(str(err))
+
+
+def get_password(config, params):
+    try:
+        cyber_ark = CyberARK(config)
+        endpoint = operations['get_credentials'][1].format(config.get('AppID'), config.get('Safe'))
+        additional_attributes = params.get('additional_attributes')
+        if additional_attributes:
+            params.update(params.pop('additional_attributes'))
+        params = {k: v for k, v in params.items() if v is not None and v != ''}
+        retrieve_creds = cyber_ark.make_request_call(endpoint, method='GET', query_params=params)
+        return retrieve_creds
+    except Exception as err:
+        logger.exception("{0}".format(str(err)))
+        raise ConnectorError("{0}".format(str(err)))
+
+
+def get_credentials(config, params):
+    cyber_ark = CyberARK(config)
+    endpoint = operations['get_credentials'][1].format(config.get('AppID'), config.get('Safe'))
+    get_account_det = cyber_ark.make_request_call(endpoint, method='GET')
+    formatted_output = []
+    user_name = get_account_det.get('UserName')
+    if user_name:
+        formatted_output.append(
+            {
+                "key": user_name,
+                "display_name": user_name
+            }
+        )
+    else:
+        formatted_output.append(
+            {
+                "key": config.get('AppID'),
+                "display_name": config.get('AppID')
+            }
+        )
+    return formatted_output
+
+
+def get_credentials_details(config, params):
+    cyber_ark = CyberARK(config)
+    Object = params.get('secret_id')
+    endpoint = operations['get_credentials_details'][1].format(config.get('AppID'), config.get('Safe'), Object)
+    get_account_det = cyber_ark.make_request_call(endpoint, method='GET')
+    formatted_output = []
+    list_items = list(get_account_det.keys())
+    for item in list_items[:8]:
+        formatted_output.append(
+            {
+                "field_name": item,
+                "value": "*****"
+            }
+        )
+    return formatted_output
+
+
+def get_credential(config, params):
+    cyber_ark = CyberARK(config)
+    Object = params.get('secret_id')
+    endpoint = operations['get_credentials_details'][1].format(config.get('AppID'), config.get('Safe'), Object)
+    get_account_det = cyber_ark.make_request_call(endpoint, method='GET')
+    attribute_name = params.get('attribute_name')
+    if attribute_name in get_account_det:
+        return {
+            "password": get_account_det.get(attribute_name)
+        }
+    else:
+        return {
+            "message": "Invalid Attribute"
+        }
+
+
+def _check_health(config):
+    try:
+        response = get_password(config, params={})
+        if response:
+            return True
+    except Exception as err:
+        logger.exception("{0}".format(str(err)))
+        raise ConnectorError("{0}".format(str(err)))
+
+
+operations = {
+    'get_credentials': [get_credentials, '/AIMWebService/api/Accounts?AppID={0}&Safe={1}'],
+    'get_credentials_details': [get_credentials_details, '/AIMWebService/api/Accounts?AppID={0}&Safe={1}&Object={2}'],
+    'get_credential': [get_credential, '/AIMWebService/api/Accounts?AppID={0}&Safe={1}&Object={2}']
+}
---
+++ docs/CyberArkCPP.md
@@ -0,0 +1,70 @@
+## About the connector
+CyberArk Application Identity Manager (AIM) is a key component in CyberArk's Privileged Access Security suite. It helps manage and secure credentials used by applications and services by providing secure retrieval of passwords and other sensitive data.
+<p>This document provides information about the CyberArk AIM Connector, which facilitates automated interactions, with a CyberArk AIM server using FortiSOAR&trade; playbooks. Add the CyberArk AIM Connector as a step in FortiSOAR&trade; playbooks and perform automated operations with CyberArk AIM.</p>
+
+### Version information
+
+Connector Version: 1.0.0
+
+
+Authored By: Fortinet
+
+Certified: No
+
+## Installing the connector
+<p>Use the <strong>Content Hub</strong> to install the connector. For the detailed procedure to install a connector, click <a href="https://docs.fortinet.com/document/fortisoar/0.0.0/installing-a-connector/1/installing-a-connector" target="_top">here</a>.</p><p>You can also use the <code>yum</code> command as a root user to install the connector:</p>
+<pre>yum install cyops-connector-cyberark-aim</pre>
+
+## Prerequisites to configuring the connector
+- You must have the credentials of CyberArk AIM server to which you will connect and perform automated operations.
+- The FortiSOAR&trade; server should have outbound connectivity to port 443 on the CyberArk AIM server.
+
+## Minimum Permissions Required
+- Not applicable
+
+## Configuring the connector
+For the procedure to configure a connector, click [here](https://docs.fortinet.com/document/fortisoar/0.0.0/configuring-a-connector/1/configuring-a-connector)
+### Configuration parameters
+<p>In FortiSOAR&trade;, on the Connectors page, click the <strong>CyberArk AIM</strong> connector row (if you are in the <strong>Grid</strong> view on the Connectors page) and in the <strong>Configurations</strong> tab enter the required configuration details:</p>
+<table border=1><thead><tr><th>Parameter</th><th>Description</th></tr></thead><tbody><tr><td>Server URL</td><td>URL of the CyberArk server to which you will connect and perform automated operations.
+</td>
+</tr><tr><td>Application ID</td><td>Application ID that has been issued to you by CyberArk, which is used for the password retrieval process.See the "Creating an application in CyberArk" section for the procedure on how to create an application in CyberArk.
+</td>
+</tr><tr><td>Safe Name</td><td>Name of the Safe that stores the credentials, including passwords.
+</td>
+</tr><tr><td>Authentication Type</td><td>Specify the type of authentication of the CyberArk server to which you will connect and perform automated operations. By default, this option is set as "No Auth".
+<br><strong>If you choose 'Basic Auth'</strong><ul><li>Username: Specify the username to access the CyberArk server to connect and perform automated operations.</li><li>Password: Specify the password to access the CyberArk server to connect and perform automated operations.</li></ul></td>
+</tr><tr><td>Verify SSL</td><td>Specifies whether the SSL certificate for the server is to be verified or not. <br/>By default, this option is set to True.</td></tr>
+</tbody></table>
+
+## Actions supported by the connector
+The following automated operations can be included in playbooks and you can also use the annotations to access operations from FortiSOAR&trade; release 4.10.0 and onwards:
+<table border=1><thead><tr><th>Function</th><th>Description</th><th>Annotation and Category</th></tr></thead><tbody><tr><td>Get Password</td><td>Enables applications to retrieve passwords from the Central Credential Provider.</td><td>get_password <br/>Investigation</td></tr>
+</tbody></table>
+
+### operation: Get Password
+#### Input parameters
+<table border=1><thead><tr><th>Parameter</th><th>Description</th></tr></thead><tbody><tr><td>Folder</td><td>Specifies the name of the folder where the password is stored. Default, it set as "Root".
+</td></tr><tr><td>Object</td><td>Specifies the name of the password object to retrieve.
+</td></tr><tr><td>UserName</td><td>Defines search criteria according to the UserName account property.
+</td></tr><tr><td>Address</td><td>Defines search criteria according to the Address account property.
+</td></tr><tr><td>PolicyID</td><td>Defines the format that will be used in the setPolicyID method.
+</td></tr><tr><td>Additional Properties</td><td>(Optional) Specify the additional properties, in the JSON format, that you want to retrieve the password from Central Credential Provider. The additional properties signify additional fields associated with the application.
+</td></tr></tbody></table>
+
+#### Output
+The output contains the following populated JSON schema:
+
+<pre>{
+    "Content": "",
+    "UserName": "",
+    "Address": "",
+    "Database": "",
+    "PasswordChangeInProcess": ""
+}</pre>
+## Included playbooks
+The `Sample - CyberArk AIM - 1.0.0` playbook collection comes bundled with the CyberArk AIM connector. These playbooks contain steps using which you can perform all supported actions. You can see bundled playbooks in the **Automation** > **Playbooks** section in FortiSOAR&trade; after importing the CyberArk AIM connector.
+
+- Get Password
+
+**Note**: If you are planning to use any of the sample playbooks in your environment, ensure that you clone those playbooks and move them to a different collection since the sample playbook collection gets deleted during connector upgrade and delete.
